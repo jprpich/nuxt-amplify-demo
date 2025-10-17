f0932c39e91270e1c8fe5c56c118bf6e78b0d420
Joshua <joshuaprpich@gmail.com>
2025-10-17 13:39:28 -0500

    add products page

---
+++ pages/products/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h2>Products Page</h2>  
+    </div>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  <style scoped>
+  h2 {
+    color: red;
+  }
+  p {
+    color: blue;
+  }
+  </style>
