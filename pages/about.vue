d2765b7ae8da9e741aabfad63f6feb2704d122f4
Joshua <joshuaprpich@gmail.com>
2025-10-17 13:34:21 -0500

    initial commit

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h2>About Page</h2>
+      <p>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+      <p>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+      <p>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p> 
+  
+    </div>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  <style scoped>
+  h2 {
+    color: red;
+  }
+  p {
+    color: blue;
+  }
+  </style>
